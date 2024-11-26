acb0d1b017c44fb4b6dab7e467365608d6573952
ArushiG11 <161923222+ArushiG11@users.noreply.github.com>
2024-11-25 23:56:04 -0500

    Add files via upload

--- WildCardMatching.py
+++ WildCardMatching.py
@@ -54,3 +54,47 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+
+
+def wildcardMatching(pattern, text):
+
+    # def isAllStars(S1, i):
+    # # Helper function to check if all characters up to index i in S1 are '*'
+    #     for j in range(i + 1):
+    #         if S1[j] != '*':
+    #             return False
+    #     return True
+
+    # def matchChars(i,j,dp):
+    #     if i<0 and j<0:
+    #         return True
+    #     if i<0 and j>=0:
+    #         return False
+    #     if i>=0 and j<0:
+    #         return isAllStars(pattern, i)
+    #     if dp[i][j] !=-1:
+    #         return dp[i][j]
+    #     if pattern[i] == text[j] or pattern[i]== '?':
+    #         dp[i][j] = matchChars(i-1,j-1, dp)
+    #     elif pattern[i] == '*':
+    #         dp[i][j] = matchChars(i-1,j, dp) or matchChars(i,j-1, dp)
+    #     else:
+    #         dp[i][j]= False
+
+    #     return dp[i][j]
+
+    #m = len(pattern)
+    #n = len(text)
+    # dp = [[-1 for _ in range(n)] for _ in range(m)]
+    # return matchChars(m-1,n-1,dp)
+
+    dp = [[False for _ in range(n+1)] for _ in range(m+1)]
+    dp[0][0] = True
+    for j in range(1,n):
+        dp[0][j] = False
+    for i in range(1,m):
+        
+    
+    
+
